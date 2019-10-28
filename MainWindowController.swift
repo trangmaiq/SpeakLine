57e42c0e02b7b5f0fe1474421ac32dfdea36a5f0
Trang <40815729+trangmaiq@users.noreply.github.com>
2019-10-28 17:46:29 +0700

    Working with delegation (#3)

--- MainWindowController.swift
+++ MainWindowController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate, NSWindowDelegate {
     
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var speakButton: NSButton!
@@ -22,11 +22,18 @@ class MainWindowController: NSWindowController {
             updateButtons()
         }
     }
+    
+    let voices = NSSpeechSynthesizer.availableVoices
 
     override func windowDidLoad() {
         super.windowDidLoad()
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
         updateButtons()
+        
+        // Set the delegate property of speech synthesizer to the MainWindowController
+        // Note that NSSpeechSynthesizer's delegate is not defined as a strong reference. This is true of all delegate properties and
+        // prevents a strong reference cycle from occuring between the delegate and object with the delegate.
+        speechSynth.delegate = self
     }
     
     override var windowNibName: NSNib.Name {
@@ -38,7 +45,7 @@ class MainWindowController: NSWindowController {
         // Get typed-in text as a string
         let str = textField.stringValue
         if str.isEmpty {
-            print("string from \(textField) is empty")
+            print("string from \(String(describing: textField)) is empty")
         } else {
             speechSynth.startSpeaking(str)
             isStarted = true
@@ -47,10 +54,20 @@ class MainWindowController: NSWindowController {
     
     @IBAction func stop(sender: NSButton) {
         speechSynth.stopSpeaking()
-        isStarted = false
     }
     
-    private func updateButtons() {
+    // MARK: - NSWindowDelegate
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        return !isStarted
+    }
+    
+    func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
+        isStarted = false
+        print("finishedSpeaking = \(finishedSpeaking)")
+    }
+    
+    
+    func updateButtons() {
         if isStarted {
             speakButton.isEnabled = false
             stopButton.isEnabled = true
@@ -59,5 +76,4 @@ class MainWindowController: NSWindowController {
             stopButton.isEnabled = false
         }
     }
-    
 }
